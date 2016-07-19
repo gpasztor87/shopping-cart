1af4bb81934c5008c348daa95244f4c966a24df3
Pásztor Gábor <gpasztor87@gmail.com>
2016-07-19 20:50:56 +0200

    Route grouping and protection

--- routes/user.js
+++ routes/user.js
@@ -6,20 +6,29 @@ var passport = require('passport');
 var csrfProtection = csrf();
 router.use(csrfProtection);
 
+router.get('/profile', isLoggedIn, function(req, res, next) {
+    res.render('user/profile');
+});
+
+router.get('/logout', isLoggedIn, function(req, res, next) {
+    req.logout();
+    res.redirect('/');
+});
+
+router.use('/', notLoggedIn, function(req, res, next) {
+    next();
+});
+
 router.get('/signup', function(req, res, next) {
     var messages = req.flash('error');
     res.render('user/signup', { csrfToken: req.csrfToken, messages: messages, hasErrors: messages.length > 0 });
 });
 
 router.post('/signup', passport.authenticate('local.signup', {
-    successRedirect: '/profile',
-    failureRedirect: '/signup',
+    successRedirect: '/user/profile',
+    failureRedirect: '/user/signup',
     failureFlash: true
 }));
-
-router.get('/profile', function(req, res, next) {
-    res.render('user/profile');
-});
 
 router.get('/signin', function(req, res, next) {
     var messages = req.flash('error');
@@ -27,9 +36,25 @@ router.get('/signin', function(req, res, next) {
 });
 
 router.post('/signin', passport.authenticate('local.signin', {
-    successRedirect: '/profile',
-    failureRedirect: '/signin',
+    successRedirect: '/user/profile',
+    failureRedirect: '/user/signin',
     failureFlash: true
 }));
 
 module.exports = router;
+
+function isLoggedIn(req, res, next) {
+    if (req.isAuthenticated()) {
+        return next();
+    }
+
+    res.redirect('/');
+}
+
+function notLoggedIn(req, res, next) {
+    if (!req.isAuthenticated()) {
+        return next();
+    }
+
+    res.redirect('/');
+}
